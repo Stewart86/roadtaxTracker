8060a9728b1acef22c5bc403fe715ec7bfc80013
stewart86 <siwei.wong@gmail.com>
2018-04-24 17:21:22 +0800

    minor cosmetic change

--- view.py
+++ view.py
@@ -2,7 +2,7 @@
 from tkinter import (BooleanVar, Button, Checkbutton, Entry, Frame, Label,
                      Scrollbar, Tk, Toplevel, messagebox)
 from tkinter.constants import (ACTIVE, BOTH, CENTER, DISABLED, LEFT, NORMAL,
-                               RIGHT, TOP, W)
+                               RIGHT, TOP, W, E, N, S)
 
 from controller import (add_new, delete_item, show_within, sort_show_vehicle,
                         update_checks)
@@ -21,20 +21,18 @@ def SecWindow():
     # Implement autocomplet on vehicle
     # instead of second window, make a pane or notbook for editing
     edit_window.add_vehlabel = Label(edit_window, text='Vehicle no.')
-    edit_window.add_vehlabel.pack()
-
+    edit_window.add_vehlabel.grid(row=0, column=0)
     sorted_list = sort_show_vehicle()
-
     edit_window.add_vehicle = Combobox_Autocomplete(edit_window, sorted_list)
-    edit_window.add_vehicle.pack(padx=15)
-
+    edit_window.add_vehicle.grid(row=0, column=0)
+    
     edit_window.add_explabel = Label(
         edit_window, text='expiry date in (dd.mm.yyyy)')
-    edit_window.add_explabel.pack()
-
+    edit_window.add_explabel.grid(row=0, column=0)
+    
     edit_window.add_expiry = Entry(edit_window)
-    edit_window.add_expiry.pack(padx=15)
-
+    edit_window.add_expiry.grid(row=0, column=0)
+    
     def show_info(msg):
         messagebox.showinfo("This is magical!", msg)
 
@@ -55,18 +53,22 @@ def SecWindow():
 
     edit_window.delete_button = Button(
         edit_window, command=del_edit, text="delete")
-    edit_window.delete_button.pack(side=LEFT)
-
+    edit_window.delete_button.grid(row=0, column=0)
+    
     edit_window.add_button = Button(
         edit_window, command=quit_edit, text="Add / Edit")
-    edit_window.add_button.pack()
-
+    edit_window.add_button.grid(row=0, column=0)
+    
     edit_window.protocol("WM_DELETE_WINDOW", quit_edit)
 
 
 root = Tk()
 root.title("RoadTax Renewal Tracker")
 root.configure(background='lavender')
+window_width = 1080
+window_height = 600
+root.maxsize(width=window_width, height=window_height)
+root.minsize(width=window_width, height=window_height)
 
 vehicle = ""
 expiry = ""
@@ -103,11 +105,11 @@ title = Label(inputframe, font=(
     "Courier", 24), text="Road-Tax Renewal Tracker", bg='lavender')
 label = Label(inputframe, text="day(s)", bg='lavender')
 
-title.pack(side=LEFT, padx=40)
-label.pack(fill=BOTH, side=LEFT)
+title.grid(row=0, column=0, columnspan=8,padx=40)
+label.grid(row=1, column=3)
 
-inputframe.pack(padx=30, pady=30, fill=BOTH)
-tableframe.pack(padx=30, pady=30, fill=BOTH)
+inputframe.grid(row=0, column=0)
+tableframe.grid(row=1, column=0, padx=30, pady=30, sticky=N+S+E+W)
 
 # relief column defination to controller.py
 mc = Multicolumn_Listbox(tableframe, ["Vehicle Number", "Expiry Date",
@@ -118,11 +120,12 @@ mc = Multicolumn_Listbox(tableframe, ["Vehicle Number", "Expiry Date",
 scrollbar = Scrollbar(tableframe)
 scrollbar.config(command=mc.interior.yview)
 
-scrollbar.pack(fill=BOTH, side=RIGHT)
+scrollbar.grid(row=0, column=1, sticky=N+S)
+
+# mc.configure_column(width=600, minwidth=100)
 
 mc.interior.config(yscrollcommand=scrollbar.set)
-mc.interior.pack()
-
+mc.interior.grid(row=0, column=0, sticky=N+S+E+W)
 entry = Entry(inputframe)
 entry.insert(0, "30")
 entry.focus_set()
@@ -160,13 +163,13 @@ send.bind("<Button-1>", callback)
 to_edit = Button(root, text='Edit', command=SecWindow, width=15)
 to_edit.config(state=ACTIVE)
 
-
-to_edit.pack(side=TOP, anchor=W, padx=40)
-entry.pack(side=LEFT)
-send.pack(side=LEFT)
-chkbox3.pack(side=RIGHT, anchor=W, pady=1, padx=1)
-chkbox2.pack(side=RIGHT, anchor=W, pady=1, padx=1)
-chkbox1.pack(side=RIGHT, anchor=W, pady=1, padx=1)
+entry.grid(row=1, column=0, pady=1, padx=1)
+send.grid(row=1, column=7, pady=1, padx=1)
+to_edit.grid(row=2, column=0, padx=40)
+entry.grid(row=1, column=2, pady=1, padx=1)
+chkbox3.grid(row=1, column=4, pady=1, padx=1)
+chkbox2.grid(row=1, column=5, pady=1, padx=1)
+chkbox1.grid(row=1, column=6, pady=1, padx=1)
 
 
 root.mainloop()
