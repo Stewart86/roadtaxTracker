40e6e5650c8d98b3642c19017280308d48104dfd
buerj <jeanmaxime.buer.pro@gmail.com>
2024-02-26 10:25:31 +0100

    refactor(Colors): create a class to store color values

---
+++ App/GUI/Colors.py
@@ -0,0 +1,12 @@
+
+class Colors():
+
+    @staticmethod
+    def get_background_color():
+        return "DeepSkyBlue2"
+    @staticmethod
+    def get_button_color():
+        return "DeepSkyBlue3"
+    @staticmethod
+    def get_alternative_backgroud_color():
+        return "#ECECEC"
