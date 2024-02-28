dc1e84ed6b3bdec1a88cf79e0067dfbfe27df80a
buerj <jeanmaxime.buer.pro@gmail.com>
2024-02-28 14:30:44 +0100

    test(test_GUI): new tests
    I created new tests to make sure buttons work fine after the bif refactoring comming up. I need to test other part of the code because not all part of the tkinter project is covered by tests

--- test/test_GUI/test_GUI.py
+++ test/test_GUI/test_GUI.py
@@ -12,35 +12,76 @@ class TestGUI(unittest.TestCase):
     def helper_assert_children(self, children):
         output = [child for child in children]
         return output
+
     def test_window(self):
         title_output = self.app.winfo_toplevel().title()
         expected = 'RoadTax Renewal Tracker'
-        self.assertEqual(expected,title_output)
+        self.assertEqual(expected, title_output)
 
     def test_window_width(self):
         width_output = self.app.winfo_width()
         expected = 1024
-        self.assertEqual(expected,width_output)
-
+        self.assertEqual(expected, width_output)
 
     def test_window_height(self):
         height_output = self.app.winfo_height()
         expected = 686
-        self.assertEqual(expected,height_output)
+        self.assertEqual(expected, height_output)
 
     def test_title_frame(self):
         output = self.app.children["!frame"].winfo_name()
         expected = "!frame"
-        self.assertEqual(expected,output)
+        self.assertEqual(expected, output)
 
     def test_children_of_root(self):
         children = self.app.children
         output = self.helper_assert_children(children)
         expected = ['!frame', '!label', '!labelframe', '!notebook']
-        self.assertEqual(expected,output)
+        self.assertEqual(expected, output)
 
     def test_children_of_frame(self):
         children = self.app.children["!frame"].children
         output = self.helper_assert_children(children)
         expected = ['!label']
+        self.assertEqual(expected, output)
+
+    def test_children_of_notebook(self):
+        children = self.app.children["!notebook"].children
+        output = self.helper_assert_children(children)
+        expected = ['!frame']
+        self.assertEqual(expected, output)
+
+    def test_children_of_frame_notebook(self):
+        children = self.app.children["!notebook"].children["!frame"].children
+        output = self.helper_assert_children(children)
+        expected = ['!label',
+                    '!treeview',
+                    '!scrollbar',
+                    '!entry',
+                    '!checkbutton',
+                    '!checkbutton2',
+                    '!checkbutton3',
+                    '!button']
+        self.assertEqual(expected, output)
+
+    def test_frame_notebook_button(self):
+        output = self.app.children["!notebook"].children["!frame"].children["!button"].invoke()
+        expected = 'None'
+        self.assertEqual(expected,output)
+
+    def test_frame_notebook_checkbutton1(self):
+        output = self.app.children["!notebook"].children["!frame"].children["!checkbutton"].invoke()
+        expected = 'None'
+        self.assertEqual(expected,output)
+
+
+    def test_frame_notebook_checkbutton2(self):
+        output = self.app.children["!notebook"].children["!frame"].children["!checkbutton2"].invoke()
+        expected = ''
+        self.assertEqual(expected,output)
+
+
+    def test_frame_notebook_checkbutton3(self):
+        output = self.app.children["!notebook"].children["!frame"].children["!checkbutton3"].invoke()
+        expected = ''
         self.assertEqual(expected,output)
